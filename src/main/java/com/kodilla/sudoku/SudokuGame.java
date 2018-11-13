f0a7bf3fa92baaeda0f803089a75fac50c7b053d
leszrom <romanowski.leszek@gmail.com>
2018-11-13 19:38:50 +0100

    Sudoku Game

--- src/main/java/com/kodilla/sudoku/SudokuGame.java
+++ src/main/java/com/kodilla/sudoku/SudokuGame.java
@@ -1,15 +1,33 @@
 package com.kodilla.sudoku;
 
 public class SudokuGame {
+    private SudokuBoard board = new SudokuBoard();
 
     public boolean resolveSudoku() {
-        /*
-         if(user want to game again)
-         should return "true"
+        System.out.println(board);
+        System.out.println(UserInterface.INSTRUCTIONS);
+        while (!definedBoard(UserInterface.getInput())) {
+            System.out.println(board);
+        }
+        SudokuAlgorithm sudokuAlgorithm = new SudokuAlgorithm(board);
+        sudokuAlgorithm.solveSudoku();
 
-         if(user want to finish)
-         should return false
-        */
-        return true;
+        return UserInterface.wantToFinish();
+    }
+
+    public boolean definedBoard(String input) {
+
+        if (input.equalsIgnoreCase(UserInterface.SUDOKU_PATTERN)) {
+            return true;
+        }
+
+        String[] stringsArray = input.split(",");
+        for (int i = 0; i < stringsArray.length; i += 3) {
+            int row = Integer.parseInt(stringsArray[i]);
+            int column = Integer.parseInt(stringsArray[i + 1]);
+            int value = Integer.parseInt(stringsArray[i + 2]);
+            board.insertValue(row, column, value);
+        }
+        return false;
     }
 }
